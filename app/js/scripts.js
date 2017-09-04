363cdac0ac2db21325d3178d5eabb37870441787
Matt Windle <mattwindle@Matts-MacBook-Pro.local>
2017-09-04 13:32:39 +0100

    mobile menu

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,84 +1,37 @@
-(function($) {
-  "use strict";
 
+$(document).ready(function(){
 
-
-  // ======================================
-  //    Smooth Scroll to element
-  // ======================================
-  $(function() {
-    var $scrollTo = $('.scroll-to');
-    $scrollTo.on('click', function(event){
-      var $elemOffsetTop = $(this).data('offset-top');
-      $('html').velocity("scroll", { offset:$(this.hash).offset().top-$elemOffsetTop, duration: 1500, easing:'easeInOutCubic', mobileHA: false});
-      event.preventDefault();
-    });
+$('.nav-trigger').click(function(){
+  $('.nav-bar').toggleClass('active');
+  $('.nav-bar img').toggleClass('show');
+});
+$('.nav-bar img').click(function(){
+  $('.nav-bar').toggleClass('active');
+  $('.nav-bar img').toggleClass('show');
 });
 
-  // ======================================
-  //    Smooth scroll to top button
-  // ======================================
-  $(function() {
-    // Animated Scroll to Top Button
-    var $scrollTop = $('.scroll-to-top-btn');
-    if ($scrollTop.length > 0) {
-      $(window).on('scroll', function(){
-        if ($(window).scrollTop() > 600) {
-          $scrollTop.addClass('visible');
-        } else {
-          $scrollTop.removeClass('visible');
-        }
-      });
-      $scrollTop.on('click', function(e){
-        e.preventDefault();
-        $('html').velocity("scroll", { offset: 0, duration: 1500, easing:'easeInOutCubic', mobileHA: false });
-      });
-    };
-});
+  $('.menu li.menu-item-has-children').on('click', function(event) {
+    if($(window).width() < 915){
+      $(this).children('ul').slideToggle();
+      $(this).parent().toggleClass('dropdown--active');
 
-
-  //------------------------------------- START: Mobile navigation setup ------------------------------------------------//
-
-  $(function() {
-
-    $(window).resize(function(){
-
-      if ($(window).width() >= 1024) {
-        $('.mobile-nav').css('display', 'none');
-        $('.site-nav__items').css('display', 'block');
-
-        if ($('.mobile-nav').hasClass('open-nav')) {
-          $('.mobile-nav').removeClass('open-nav');
-        }
-      } else if ($(window).width() < 1024) {
-        $('.mobile-nav').css('display', 'block');
-        $('.site-nav__items').css('display', 'none');
-      }
-
-    });
-
-    $('.mobile-nav').on('click', function() {
-
-    	if (!$(this).hasClass('open-nav')) {
-    		$(this).addClass('open-nav');
-    		toggleNav(true);
-    	} else {
-    		$(this).removeClass('open-nav');
-    		toggleNav(false);
-    	}
-
-    });
-
-    function toggleNav(bool) {
-
-    	if (bool === true) $('.site-nav__items').slideDown(300);
-    	else $('.site-nav__items').slideUp(300);
-
+      // if($('.menu').hasClass('dropdown--active')){
+      //   $('.menu li.menu-item-has-children').toggleClass('active;')
+      // }
+      // else {
+      //   $('.menu li.menu-item-has-children').toggleClass('active;')
+      // }
     }
-
   });
 
-  //------------------------------------- END: Mobile navigation setup ------------------------------------------------//
+  $(".menu li.menu-item-has-children").hover(function(e){
+    if($(window).width() > 915){
+      $(this).children('ul').fadeToggle();
+      $(this).toggleClass('dropdown--active');
+    }
+  });
+  e.preventDefault();
 
 
-  })(jQuery);
+
+});
--- dist/index.php
+++ dist/index.php
@@ -1,7 +1,23 @@
 <?php include 'includes/header.php' ?>
 
-  <div class="site-container">
-    <div class="example"></div>
-  </div>
+<div class="header-items" >
+  <img src="/images/crane-images/logo.png" class="logo" >
+  <div class="nav-trigger"> <img src="/images/crane-images/menu.svg" ></div>
+  <nav class="nav-bar ">
+    <img src="/images/crane-images/close.svg" >
+    <a href="#" >Home</a>
+    <a href="#" >About</a>
+    <a href="#" class="has-dropdown">Conditions</a>
+      <ul class="dropdown">
+        <li><a href="#" >Home</a></li>
+        <li><a href="#" >Home</a></li>
+        <li><a href="#" >Home</a></li>
+        <li><a href="#" >Home</a></li>
+      </ul>
+    <a href="#" >Services</a>
+    <a href="#" >Book</a>
+    <a href="#" >Contact</a>
+  </nav>
+</div>
 
 <?php include 'includes/footer.php' ?>
