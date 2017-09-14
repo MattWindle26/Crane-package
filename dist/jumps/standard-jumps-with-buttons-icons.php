db148518659768533be36cd134789be44117904f
Matt Windle <mattwindle@Matts-MacBook-Pro.local>
2017-09-14 09:38:54 +0100

    - created jumps with icons

--- dist/jumps/standard-jumps-with-buttons-icons.php
+++ dist/jumps/standard-jumps-with-buttons-icons.php
@@ -7,6 +7,9 @@ $path = $_SERVER['DOCUMENT_ROOT'];
   <a href="#" class="single-jump" >
     <img src="http://via.placeholder.com/350x350" alt="image alt text" >
     <div class="jump-content" >
+      <div class="jump-icon" >
+        <img src="/images/crane-images/email-white.svg" alt="icon alt text" >
+      </div>
       <h3>Condition</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim.</p>
       <div class="jump-btn" >Find out More</div>
@@ -15,6 +18,9 @@ $path = $_SERVER['DOCUMENT_ROOT'];
   <a href="#" class="single-jump" >
     <img src="http://via.placeholder.com/350x350" alt="image alt text" >
     <div class="jump-content" >
+      <div class="jump-icon" >
+        <img src="/images/crane-images/email-white.svg" alt="icon alt text" >
+      </div>
       <h3>Condition</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim.</p>
       <div class="jump-btn" >Find out More</div>
@@ -23,6 +29,9 @@ $path = $_SERVER['DOCUMENT_ROOT'];
   <a href="#" class="single-jump" >
     <img src="http://via.placeholder.com/350x350" alt="image alt text" >
     <div class="jump-content" >
+      <div class="jump-icon" >
+        <img src="/images/crane-images/email-white.svg" alt="icon alt text" >
+      </div>
       <h3>Condition</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim.</p>
       <div class="jump-btn" >Find out More</div>
